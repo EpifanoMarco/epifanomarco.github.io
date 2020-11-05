d50ea02f1f76d590c717268d6b46f4f1edb86f04
EpifanoMarco <73348784+EpifanoMarco@users.noreply.github.com>
2020-11-05 18:51:55 +0100

    Update Triangle_de_Sierpinski.py

--- Triangle_de_Sierpinski.py
+++ Triangle_de_Sierpinski.py
@@ -1,42 +1,49 @@
 import turtle
 turtle.tracer(0,0)            # accélération du tracé
-turtle.screensize(500,500)  # taille fenêtre graphique
+turtle.screensize(4000,4000)  # taille fenêtre graphique
 turtle.pu()
-turtle.goto(-0,-200)
+turtle.goto(-150,-150)
 turtle.pd()
 
 
 def dessiner(courbe, longueur, angle):    
-    #""" réalise une représentation graphique d'une courbe donnée par des chaines de caractères """
+    """ réalise une représentation graphique d'une courbe donnée par des chaines de caractères """
     for caractere in courbe:
         if caractere == '+': turtle.left(angle)
         elif caractere == '-': turtle.right(angle)
-        elif caractere == 'F': turtle.forward(longueur)
+        elif caractere in ['F', 'G']: turtle.forward(longueur)
 
-def dessine_A(longueur, angle): 
-  Sequence_A = 'FF++FF++FF++F+F++F++F+'
-  dessiner(Sequence_A,longueur,angle)
 
-def dessine_B(longueur, angle):
-  Sequence_B = 'F++FF'
-  dessiner(Sequence_B,longueur,angle)
+def regleSierpinski(chaine):
+    nouvelleChaine = ""    
+    for lettre in chaine:  
+        if lettre == "F":  
+            nouvelleChaine = nouvelleChaine + 'F-G+F+G-F'  
+        elif lettre == "G":
+            nouvelleChaine = nouvelleChaine + 'GG'
+        else :
+            nouvelleChaine = nouvelleChaine + lettre 
+    return nouvelleChaine
 
-def Prebienski(counter,longueur, angle):
-  dessine_A(longueur,angle)
-  for i in range(3):
-    if counter>1:
-      Prebienski(counter-1,longueur*0.5,angle)
-  dessine_B(longueur,angle)
-  return counter-1
 
-longueur = 128
-angle = 60
-niter = 5
+def courbeSierpinski(motifInitial, niter):
+    """ 
+        appelle niter fois regleSierpinski pour créer la courbe de Sierpinski
+    """
+    courbe = motifInitial 
+    for i in range(niter):
+        nouveauMotif = regleSierpinski(courbe)  
+        courbe = nouveauMotif 
+    return courbe
 
-counter = niter;
 
-if counter >= 0:
-  counter = Prebienski(counter, longueur, angle)
+longueur = 32
+angle = -120
+niter = 4
 
-turtle.update()      # accélération du tracé
-turtle.exitonclick() # permet la fermeture de la fenêtre graphique
+
+dessiner(courbeSierpinski('F-G-G', niter), longueur, angle)
+
+
+turtle.update()    
+turtle.exitonclick() 
